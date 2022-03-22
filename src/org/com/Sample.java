fe0c8930b22013a8df558d91beec666d98182c24
leojaison <jaisonleo24@gmail.com>
2022-03-22 13:40:14 +0530

    done

---
+++ src/org/com/Sample.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Sample {
+	private void name() {
+
+		System.out.println("leo");
+
+	}
+
+	public static void main(String[] args) {
+		Sample a = new Sample();
+		a.name();
+	}
+
+}
